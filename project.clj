caa45566cb304e83696ac8b9e04e2377e7d26af5
James Conroy-Finn <git@invetica.co.uk>
2016-11-16 11:50:34 +0000

    Move project into my personal namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dorothy "0.0.7-SNAPSHOT"
+(defproject jcf/dorothy "0.0.7-SNAPSHOT"
   :description "Hiccup-style generation of Graphviz graphs"
   :url "https://github.com/daveray/dorothy"
   :license {:name "Eclipse Public License - v 1.0"
